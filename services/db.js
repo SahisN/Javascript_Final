b1c67babf3c2c1764051857650265f50402d5228
sahis2002 <89941586+SahisN@users.noreply.github.com>
2024-05-10 17:37:12 -0700

    updated db.js

--- services/db.js
+++ services/db.js
@@ -1,76 +1,92 @@
-import fs from 'fs';
-import path from 'path';
-import url from 'url';
+import dotenv from 'dotenv'
+import { MongoClient, ServerApiVersion } from 'mongodb'
 
-// in ECMAScript Modules (ESM), __dirname is not available directly like in CommonJS
-// use 'url' and 'path' modules to achieve similar functionality
-const __filename = url.fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
+export default class MongoDB {
+    // get .env creds using constructor
+    constructor() {
+        dotenv.config();
 
-// define the path to the mock database directory
-const dbDirectory = path.resolve(__dirname, 'mock_database');
+        // all the .env variables
+        const DB_USER = process.env.DB_USER;
+        const DB_PASSWORD = process.env.DB_PASSWORD;
+        const DB_HOST = process.env.DB_HOST;
+        const DB_NAME = process.env.DB_NAME;
 
-/**
- * Reads and parses JSON data from a file
- * @param {string} collection - the name of the collection file
- * @returns {Promise<Array|Object>} the parsed JSON data from the collection
- * @throws {Error} an error if there's an issue reading or parsing the data
- */
-const _read = async (collection) => {
-    try {
-        const fullPath = path.resolve(dbDirectory, `${collection}.json`);
-        const data = await fs.promises.readFile(fullPath, 'utf-8');
-        console.log(data);
-        return JSON.parse(data);
-    } catch (error) {
-        console.error(error);
-        throw new Error(
-            `Error reading data from collection ${collection}: ${error.message}`
-        );
+        // database uri
+        this.mongoURL = `mongodb+srv://${DB_USER}:${DB_PASSWORD}@${DB_HOST}/${DB_NAME}?retryWrites=true&w=majority&appName=Cluster0`;
+        this.client = new MongoClient(this.mongoURL, {
+            serverApi: {
+                version: ServerApiVersion.v1,
+                strict: true,
+                deprecationErrors: true,
+            }
+        });
+        
+        this.db = null;
     }
-};
 
-/**
- * Creates a new entry in a collection
- * @param {string} collection - the name of the collection file
- * @param {Object} data - the data to be added to the collection
- * @returns {Promise<void>} a Promise that resolves when the operation is complete
- * @throws {Error} an error if there's an issue creating the record
- */
-export const create = async (collection, data) => {
-    try {
-        const records = await _read(collection);
-        records.push(data);
-
-        const fullPath = path.resolve(dbDirectory, `${collection}.json`);
-        await fs.promises.writeFile(fullPath, JSON.stringify(records));
-    } catch (error) {
-        throw new Error(
-            `Error creating record in collection ${collection}: ${error.message}`
-        );
-    }
-};
-
-/**
- * Finds all records or a record by ID in a collection
- * @param {string} collection - the name of the collection file
- * @param {string|null} id - the id of the record to find. if null, all records will be returned
- * @returns {Promise<Array|Object|null>} the record(s) found in the collection
- * @throws {Error} an error if there's an issue finding the record(s)
- */
-export const find = async (collection, id = null) => {
-    try {
-        const records = await _read(collection);
-
-        if (id) {
-            const record = records.find((record) => record.id === id);
-            return record;
-        } else {
-            return records;
+    // makes connection with the database
+    async connect() {
+        try {
+           await this.client.connect();
+           this.db = this.client.db();
+           console.log('Connected');
         }
-    } catch (error) {
-        throw new Error(
-            `Error finding record in collection ${collection}: ${error.message}`
-        );
+
+        catch(error) {
+            console.log('Error connecting: ', error);
+        }
     }
-};
+
+    // closes connections with the database
+    async close() {
+        try {
+            await this.client.close();
+            console.log('Connection closed');
+        }
+
+        catch(error) {
+            console.error('Error closing connection: ', error)
+        }
+    }
+ 
+    // creates a collection in the db
+    async create(collectionName, data) {
+        try {
+            const collection = this.db.collection(collectionName);
+            const result = await collection.insertOne(data);
+            console.log('Item inserted: ', result.insertedId);
+            
+            return result;
+        }
+        
+        catch(error) {
+            console.error('Error inserting data: ', error)
+        }
+    }
+
+    // finds element in collection by a valid id, otherwise returns the whole collection
+    async find(collectionName, id) {
+        try {
+            const collection = this.db.collection(collectionName);
+            
+            // if id is valid then return element 
+            if (id) 
+            {
+                const cursor = await collection.find({id}).toArray();
+                return cursor
+            }
+            
+            // if id is not valid, return the collection
+            else 
+            {
+                const cursor = await collection.find({}).toArray();
+                return cursor;
+            }
+        }
+
+        catch(error) {
+            console.error('Error finding data:', error)
+        }
+    }
+}
