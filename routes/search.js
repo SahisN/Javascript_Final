db4558c3b8c6d39264fa65283c081a0b947c51a5
SahisN <89941586+SahisN@users.noreply.github.com>
2024-05-13 15:57:41 -0700

    Update search.js

--- routes/search.js
+++ routes/search.js
@@ -1,30 +1,31 @@
 import express from 'express';
-import { searchByKeyword } from '../services/api.js';
-import { db } from '../server.js';
+import * as api from '../services/api.js'; 
+import MongoDB from '../services/db.js';
 
 const router = express.Router();
+const mongo = new MongoDB();
+await mongo.connect();
 
-router.get('/search', async (req, res) => {
+router.get('/character', async (req, res) => {
     try {
         const { query } = req;
-        const results = await searchByKeyword(query.searchTerm);
-        const filter = { searchTerm: query.searchTerm };
+        const results = await api.keyword_search(query.name);
 
         const document = {
-            searchTerm: query.searchTerm,
-            searchCount: results.length,
+            searchTerm: query.name,
+            searchCount: results.info.count,
             lastSearched: new Date()
         };
 
-        const cursor = await db.find('search_history', filter);
-        const arr = await cursor.toArray();
 
-        if (arr.length) {
-            await db.update('search_history', filter, {
+        const cursor = await mongo.find('search_history', document.searchTerm);
+ 
+        if (cursor.length) {
+            await mongo.update('search_history', id, {
                 lastSearched: new Date()
             });
         } else {
-            await db.create('search_history', document);
+            await mongo.create('search_history', document);
         }
         res.json(document);
     } catch (error) {
@@ -32,9 +33,42 @@ router.get('/search', async (req, res) => {
     }
 });
 
-export default router;
 
+router.get('/:id/details/', async (req,res) => {
+    try{
+        const { params, query } = req; 
+        const characterId  = params.id;
+        const { cache }  = query;
 
+        let searchChar;
+        let result;
 
+        if(cache === 'true') {
+            const cacheResult = await mongo.find('search_cache', characterId);
 
-// code
+            if(!cacheResult.length){
+                searchChar = await api.details(characterId);
+                result = {id: characterId, name: searchChar.data.name, films: searchChar.data.films, videogames: searchChar.data.videoGames}
+                await mongo.create('search_cache', result);         
+            } else {
+                result = cacheResult;
+            }
+
+        } else {
+            const checkCollection = await mongo.find('search_cache', characterId);
+            if (checkCollection.length) {
+                result = checkCollection;
+            } else {
+                const searchChar = await api.details(characterId);
+                result = { id: characterId, name: searchChar.data.name, films: searchChar.data.films, videogames: searchChar.data.videoGames };
+                await mongo.create('search_cache', result);
+            }
+        }
+   
+        res.json(result);
+    } catch (err) {
+        res.status(500).json({ err });
+    }
+});
+
+export default router; 
